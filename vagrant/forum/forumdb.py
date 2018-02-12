1555bb1489eec4cb3f63d4b0cde6d883c48c1997
Maria Sorokina <fftpm141181033@ya.ru>
2018-02-12 19:33:42 +0300

    Change forum db file so it could use db instead of lists

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,23 @@
 # "Database code" for the DB Forum.
 
-import datetime
+import psycopg2 as pg
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+DBNAME = "forum"
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  db = pg.connect("dbname=" + DBNAME)
+  c = db.cursor()
+  c.execute("select content, time from posts order by time desc;")
+  data = c.fetchall()
+  db.close()
+  return data
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
+  db = pg.connect("dbname=" + DBNAME)
+  c = db.cursor()
+  c.execute('''insert into posts values ('%s');''' % content)
+  db.commit()
+  db.close()
 
